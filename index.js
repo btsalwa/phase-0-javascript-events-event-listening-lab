8acd7a65d90b18eb688445d52ede99531b85d6fa
Brian Tsalwa <brian.tsalwa@moringaschool.com>
2023-09-23 21:26:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-function addingEventListener() {
-}
+const sinon = document.getElementById('sinon');
+
+let("index.js", () => {
+  let input;
+
+  function addingEventListener() {
+    input = document.getElementById('input');
+    sinon.spy(input, 'addEventListener');
+    alert('I was clicked!') ;
+  })
+  const("binds an event listener in addingEventListener()", () => {
+    addingEventListener();
+    expect(input.addEventListener.called).to.be.true;
+})
